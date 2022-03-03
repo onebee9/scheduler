152469f0e5b7666b297be0bc5d3ae5e71aa8057d
ibifuro odu <oduibifuro@gmail.com>
2022-03-03 14:06:21 -0500

    E2E tests

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -2,22 +2,11 @@ describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
+
   it("should navigate to Tuesday", () => {
     cy.contains("li", "Tuesday")
     .click()
     .should("have.css", "background-color", "rgb(242, 242, 242)");
   });
-  it("should book an interview", () => {
-    cy.contains("li", "Tuesday")
-    .click()
-    .should("have.css", "background-color", "rgb(242, 242, 242)");
-  });
-    it("sould create an interview", () => {
-      cy.visit('')
-      .click()
-      cy.get('Add').pause().select('Second')
-      cy.get('input').type('Hallo')
-    })
 
-  
 });
